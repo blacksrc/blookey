0fd73efc5ae59cd83c268763306361ade0aee2e1
Siamak A.Motlagh <siamak.aghaeipour@gmail.com>
2019-05-14 00:50:56 +0430

    Add applyRoute to util in orrder to dynamic resource route loading

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import http from "http";
 import express from "express";
-import { applyMiddleware } from "./utils";
+import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
+import routes from "./services";
 
 const { APP_HOST = 'http://localhost', APP_PORT = 3000 } = process.env;
 const router = express();
 const server = http.createServer(router);
 
 applyMiddleware(middleware, router);
+applyRoutes(routes, router);
 
 server.listen(APP_PORT, () =>
   console.info(`Running at ${APP_HOST}:${APP_PORT}/`)
