64f8f2f69761d451cb30abf664da42ba48ea66a8
Siamak A.Motlagh <siamak.aghaeipour@gmail.com>
2019-05-14 23:59:09 +0430

    Implement error handling functionality

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import http from "http";
 import express from "express";
 import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
+import errorHandlers from "./middleware/errorHandlers";
 import routes from "./services";
 
 const { APP_HOST = 'http://localhost', APP_PORT = 3000 } = process.env;
@@ -10,6 +11,16 @@ const server = http.createServer(router);
 
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
+applyMiddleware(errorHandlers, router);
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
 
 server.listen(APP_PORT, () =>
   console.info(`Running at ${APP_HOST}:${APP_PORT}/`)
